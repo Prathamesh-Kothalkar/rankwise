5b0e8bdf04181e63d5f347ec2c54301a827567a7
Prathamesh Kothalkar <prathameshkothalkar9021@gmail.com>
2025-06-17 21:16:07 +0530

    Update Contact

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Navbar from "@/components/Navbar"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
