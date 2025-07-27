75169a2591cf99cb5c1493a6449bb8c32f3bf79f
Prathamesh Kothalkar <prathameshkothalkar9021@gmail.com>
2025-07-27 13:38:33 +0530

    Updated Ads

--- components/Adstera/Popunder.tsx
+++ components/Adstera/Popunder.tsx
@@ -2,12 +2,12 @@
 import { useEffect } from "react";
 
 const Popunder: React.FC = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "//pl27070925.profitableratecpm.com/62/08/d9/6208d9d2b2ead70c886b98171c7d98b1.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "//pl27070925.profitableratecpm.com/62/08/d9/6208d9d2b2ead70c886b98171c7d98b1.js";
+  //   script.async = true;
+  //   document.body.appendChild(script);
+  // }, []);
 
   return null;
 };
